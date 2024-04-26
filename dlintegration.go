c07cbc9d0690bb9a9a9a4df82aa518acb63ad2e9
Maksim Pinigin <at@pinig.in>
2024-04-26 22:40:37 +0700

    Created method switch-studio and improved switch-scene

--- dlintegration.go
+++ dlintegration.go
@@ -3,14 +3,16 @@ package main
 import (
 	"log"
 	"os"
+	"time"
 
 	"github.com/andreykaipov/goobs/api/requests/inputs"
 	"github.com/andreykaipov/goobs/api/requests/scenes"
+	"github.com/andreykaipov/goobs/api/requests/ui"
 	"github.com/estintax/mpc_streamdeck/dinolang"
 )
 
 func initDL() {
-	dinolang.Classes["deck"] = dinolang.Class{
+	dinolang.Classes["deck"] = &dinolang.Class{
 		Prefix:    "deck",
 		Used:      true,
 		IsBuiltIn: false,
@@ -37,9 +39,16 @@ func DeckClassHandler(args []string, segmentName string) bool {
 		if len(args) > 1 {
 			if dinolang.GetTypeEx(args[1]) == "string" {
 				sceneName := dinolang.StringToText(dinolang.IfVariableReplaceIt(args[1]).(string))
-				params := scenes.NewSetCurrentProgramSceneParams()
-				params.SceneName = &sceneName
-				_, err := client.Scenes.SetCurrentProgramScene(params)
+				info, err := client.Ui.GetStudioModeEnabled()
+				if err == nil && info.StudioModeEnabled {
+					params := scenes.NewSetCurrentPreviewSceneParams()
+					params.SceneName = &sceneName
+					_, err = client.Scenes.SetCurrentPreviewScene(params)
+				} else {
+					params := scenes.NewSetCurrentProgramSceneParams()
+					params.SceneName = &sceneName
+					_, err = client.Scenes.SetCurrentProgramScene(params)
+				}
 				if err != nil {
 					dinolang.PrintError(err.Error())
 					dinolang.SetReturned("int", 0, segmentName)
@@ -115,6 +124,33 @@ func DeckClassHandler(args []string, segmentName string) bool {
 			dinolang.SetReturned("int", 0, segmentName)
 			return false
 		}
+	case "switch-studio":
+		info, err := client.Ui.GetStudioModeEnabled()
+		if err != nil {
+			dinolang.PrintError(err.Error())
+			dinolang.SetReturned("int", 0, segmentName)
+			return false
+		}
+		enabled := true
+		if info.StudioModeEnabled {
+			enabled = false
+			previewInfo, _ := client.Scenes.GetCurrentPreviewScene()
+			currentInfo, _ := client.Scenes.GetCurrentProgramScene()
+			if previewInfo.CurrentPreviewSceneName != currentInfo.CurrentProgramSceneName {
+				res, _ := client.Transitions.GetCurrentSceneTransition()
+				client.Scenes.SetCurrentProgramScene(&scenes.SetCurrentProgramSceneParams{SceneName: &previewInfo.CurrentPreviewSceneName})
+				time.Sleep(time.Duration(time.Duration(res.TransitionDuration)*time.Millisecond) + 500*time.Millisecond)
+			}
+		}
+		_, err = client.Ui.SetStudioModeEnabled(&ui.SetStudioModeEnabledParams{StudioModeEnabled: &enabled})
+		if err != nil {
+			dinolang.PrintError(err.Error())
+			dinolang.SetReturned("int", 0, segmentName)
+			return false
+		}
+	default:
+		dinolang.PrintError("Unknown method")
+		return false
 	}
 
 	return true
